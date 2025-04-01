8a222d9422ab69e019f8d19f46441fcae44b62b4
Tick042 <2014340257@qq.com>
2025-04-01 18:58:50 +0800

    feat(dma): Simplified some long code

--- src/main/scala/zhujiang/device/dma/AxiWrSlave.scala
+++ src/main/scala/zhujiang/device/dma/AxiWrSlave.scala
@@ -122,14 +122,27 @@ class AxiWrSlave(implicit p: Parameters) extends ZJModule with HasCircularQueueP
         e.fullWrap  := false.B
       }
   }
-  txAwBdl            := 0.U.asTypeOf(txAwBdl)
-  txAwBdl.addr       := Mux(uTailE.cache(1) & Burst.isWrap(uTailE.burst), Mux(uTailE.byteMask(rni.offset) ^  uTailE.byteMask(rni.offset - 1), Cat(uTailE.preAddr, uTailE.exAddr(rni.pageBits - 1, rni.offset), 0.U(rni.offset.W)), Cat(uTailE.preAddr, uTailE.exAddr(rni.pageBits - 1, rni.offset - 1), 0.U((rni.offset - 1).W))), Cat(uTailE.preAddr, uTailE.exAddr))
+  txAwBdl                       := 0.U.asTypeOf(txAwBdl)
+  private val specWrapModify     = uTailE.cache(1) & Burst.isWrap(uTailE.burst) & (uTailE.byteMask(rni.offset) ^  uTailE.byteMask(rni.offset - 1))
+  private val lessWrapModify     = uTailE.cache(1) & Burst.isWrap(uTailE.burst) & !uTailE.byteMask(rni.offset - 1)
+  private val specWrapModifyAddr = Cat(uTailE.preAddr, uTailE.exAddr(rni.pageBits - 1, rni.offset), 0.U(rni.offset.W))
+  private val lessWrapModifyAddr = Cat(uTailE.preAddr, uTailE.exAddr(rni.pageBits - 1, rni.offset - 1), 0.U((rni.offset - 1).W))
+  private val defaultAddr        = Cat(uTailE.preAddr, uTailE.exAddr)
+
+  txAwBdl.addr       := PriorityMux(Seq(
+    specWrapModify   -> specWrapModifyAddr,
+    lessWrapModify   -> lessWrapModifyAddr,
+    true.B           -> defaultAddr
+  ))
   txAwBdl.size       := Mux(!uTailE.cache(1) || Burst.isFix(uTailE.burst), uTailE.size, log2Ceil(dw/8).U)
   txAwBdl.cache      := uTailE.cache
   txAwBdl.burst      := Burst.INCR
   txAwBdl.id         := dHeadPtr.value
-  txAwBdl.len        := Mux(!uTailE.cache(1) | Burst.isFix(uTailE.burst) | uTailE.exAddr(rni.offset - 1) & Burst.isIncr(uTailE.burst) | Burst.isWrap(uTailE.burst) & (uTailE.exAddr(rni.offset - 1) & uTailE.byteMask(rni.offset) | !uTailE.byteMask(rni.offset - 1)) |
-                          (uTailE.exAddr(rni.pageBits - 1, rni.offset) === uTailE.endAddr(rni.pageBits - 1, rni.offset)) & (uTailE.endAddr(rni.offset - 1, 0) <= "b100000".U) & uTailE.exAddr(rni.offset - 1, 0) =/= uTailE.endAddr(rni.offset - 1, 0), 0.U, 1.U)
+
+  private val incrOffset        = uTailE.exAddr(rni.offset - 1) & Burst.isIncr(uTailE.burst)
+  private val wrapOffset        = Burst.isWrap(uTailE.burst) & (uTailE.exAddr(rni.offset - 1) & uTailE.byteMask(rni.offset) | uTailE.byteMask(rni.offset - 1))
+  private val otherHalf         = (uTailE.exAddr(rni.pageBits - 1, rni.offset) === uTailE.endAddr(rni.pageBits - 1, rni.offset)) & (uTailE.endAddr(rni.offset - 1, 0) <= "b100000".U) & uTailE.exAddr(rni.offset - 1, 0) =/= uTailE.endAddr(rni.offset - 1, 0)
+  txAwBdl.len                  := Mux(!uTailE.cache(1) | Burst.isFix(uTailE.burst) | incrOffset | wrapOffset | otherHalf, 0.U, 1.U)
 
 /* 
  * IO Connection Logic
