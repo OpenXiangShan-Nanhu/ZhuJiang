c2fcba6cc6115b7d5216d12e3754f3133b8ec246
Tick042 <2014340257@qq.com>
2025-05-08 10:08:05 +0800

    fix(dma): fix the bug of x state

--- src/main/scala/zhujiang/device/dma/ChiRdMaster.scala
+++ src/main/scala/zhujiang/device/dma/ChiRdMaster.scala
@@ -177,7 +177,7 @@ class ChiRdMaster(implicit p: Parameters) extends ZJModule with HasCircularQueue
   io.wrDB.valid               := Mux(chiEntries(dataTxnid).double, io.chiDat.valid, 
                                   Mux(fromDCT(io.chiDat.bits.SrcID), Mux(chiEntries(dataTxnid).addr(rni.offset - 1), io.chiDat.bits.DataID === 2.U & io.chiDat.valid, io.chiDat.valid & io.chiDat.bits.DataID === 0.U), io.chiDat.valid))
   if(rni.readDMT){
-    io.chiTxRsp.get.valid         := !chiEntries(txRspPtr.value).haveSendAck.get & (txRspPtr =/= rxRspPtr) & chiEntries(txRspPtr.value).rcvDatComp
+    io.chiTxRsp.get.valid         := !chiEntries(txRspPtr.value).haveSendAck.get & (txRspPtr =/= rxRspPtr) & chiEntries(txRspPtr.value).rcvDatComp & validVec(txRspPtr.value)
     io.chiTxRsp.get.bits          := txRspBdl
   } 
   io.chiDat.ready             := io.wrDB.ready
