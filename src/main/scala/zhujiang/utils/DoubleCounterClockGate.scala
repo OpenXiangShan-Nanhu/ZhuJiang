975efce0ea7d3303d95682a38a9046ae4dd23af1
梁森 Liang Sen <liangsen20z@ict.ac.cn>
2025-05-14 19:13:15 +0800

    fix(clockgate): add active signal for pwr ctl.

--- src/main/scala/zhujiang/utils/DoubleCounterClockGate.scala
+++ src/main/scala/zhujiang/utils/DoubleCounterClockGate.scala
@@ -9,16 +9,20 @@ class DoubleCounterClockGate(ckenWindow:Int = 7, idleWindow:Int = 7) extends Mod
     val inbound = Input(Bool())
     val working = Input(Bool())
     val ock = Output(Clock())
+    val active = Output(Bool())
   })
   private val ckenWindowCnt = RegInit(ckenWindow.U(log2Ceil(ckenWindow).W))
   private val idleWindowCnt = RegInit(idleWindow.U(log2Ceil(idleWindow).W))
   private val cken = RegInit(true.B)
+  private val active = RegInit(false.B)
   private val cg = Module(new xs.utils.ClockGate)
   cg.io.TE := io.te
   cg.io.CK := clock
   io.ock := cg.io.Q
   cg.io.E := cken | io.inbound
+  io.active := active
   cken := Mux(cken, ckenWindowCnt.orR || idleWindowCnt.orR, io.inbound)
+  active := Mux(active, ckenWindowCnt.orR || idleWindowCnt.orR, io.inbound)
   when(io.inbound) {
     ckenWindowCnt := Fill(ckenWindowCnt.getWidth, true.B)
   }.elsewhen(ckenWindowCnt.orR) {
